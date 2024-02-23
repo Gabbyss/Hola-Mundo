49e1dabee8e93c93bd6efcfb3c52947c6e56a739
Gabbyss <160082866+Gabbyss@users.noreply.github.com>
2024-02-23 02:07:08 +0000

    Version 2

---
+++ include/Alimento.hpp
@@ -0,0 +1,16 @@
+class Alimento
+{
+private:
+    int energia;
+public:
+    Alimento(int energia) 
+    {
+        this -> energia = energia;
+    }
+
+    ~Alimento() {}
+    int ExtraerEnergia ()
+    {
+        return energia;
+    }
+};
